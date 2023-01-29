f39b5eb2c4935b6b4985abacb41572b26430d3a8
kenetcode <k2ken2424@gmail.com>
2023-01-29 10:36:03 -0600

    Optiizando el programa para que pueda alcanzar la posicion 92 de la sucesion

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         //Programa que genere infinitos numero de la sucesión de fibonacci
-        //Primero dos terminos unicos de la sucesión
-        /*int f0 = 0, f1 = 1, f2 = 0, f3, f4, f5;
-        System.out.println(f0);
-        System.out.println(f1);
-        f2 = f1 + f0;
-        System.out.println(f2);
-        f3 = f2 + f1;
-        System.out.println(f3);
-        f4 = f3 + f2;
-        System.out.println(f4);
-        f5 = f4 + f3;
-        System.out.println(f5);*/
+        //Este programa esta optimizado actualmente solo para mostrar hasta la posicion 92 de la sucesion, ya que es lo
+        //que permite el tipo de dato long en Java
 
-        int n = 8, fib0 = 0, fib1 = 1, temp = 0, result = 0;
-        System.out.println(fib0);
-        System.out.println(fib1);
-        for (int i = 1; i < 20; i++) {
-            if (i > 0) {
-                temp = fib0;
-                fib0 = fib1;
-                fib1 = fib1 + temp;
-                n = n - 1;
-            } else {
-                result = fib0;
-            }
-            System.out.println(fib1);
+        long fib0 = 0l, fib1 = 1, result = 0, first = 0, second = 0, n = 0;
+        result = fib1 + fib0;
+        System.out.println("Fib Pos # " + n + "     " + fib0);
+        n++;
+        System.out.println("Fib Pos # " + n + "     " + fib1);
+        n++;
+        System.out.println("Fib Pos # " + n + "     " + result);
+        while (n < 92) {
+            n++;
+            second = result;
+            first = fib1;
+            result = second + first;
+            fib1 = second;
+            System.out.println("Fib Pos # " + n + "     " + result);
         }
     }
 }
